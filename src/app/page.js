9889247f88c91a7b7efaff773cc7b88fd3b9bfdf
Bertrand Wijaya <bertrandwijaya05@gmail.com>
2024-10-07 21:33:16 +0700

    Couple of changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,30 @@
-import { getPosts } from "@/_actions/postAction";
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-export default async function Home() {
-
-  const { data, errMsg } = await getPosts();
-
-  if(errMsg)
-    return <h1>{errMsg}</h1>
-
+const page = () => {
   return (
-    <main>
-      {
-        data.map(item => (
-          <h1 key={item._id}>{item.msg}</h1>
-        ))
-      }
-    </main>
-  );
+    redirect('/home')
+  )
 }
+
+export default page
+
+// import { getPosts } from "@/_actions/postAction";
+
+// export default async function Home() {
+
+//   const { data, errMsg } = await getPosts();
+
+//   if(errMsg)
+//     return <h1>{errMsg}</h1>
+
+//   return (
+//     <main>
+//       {
+//         data.map(item => (
+//           <h1 key={item._id}>{item.msg}</h1>
+//         ))
+//       }
+//     </main>
+//   );
+// }
