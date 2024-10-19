3afb0efe5ec7f2235a8ae1842b4f65238d89f7fe
Bertrand Wijaya <bertrandwijaya05@gmail.com>
2024-10-19 17:51:53 +0700

    Fix donations TYPO

--- src/app/api/donation/route.js
+++ src/app/api/donation/route.js
@@ -8,7 +8,7 @@ export const GET = async(request) => {
         await connectToDB();
         const donations = await Donation.find({});
 
-        return new Response(JSON.stringify(dontaions), { status: 200 });
+        return new Response(JSON.stringify(donations), { status: 200 });
     } catch (error) {
         return new Response("Failed to fetch all donations.", { status: 500 });
     }
