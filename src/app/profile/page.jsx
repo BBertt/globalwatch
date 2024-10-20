b9a2ae631fee3d57fd16c887860a73193ab88632
Bertrand Wijaya <bertrandwijaya05@gmail.com>
2024-10-20 10:24:36 +0700

    Donation Authenticate & User Profile
    Authenticate if user is logged in when they want to make donation. User can view donations they have made in their own profile.

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,33 @@
+"use client";
+
+import UserDonationList from "@/components/UserDonationList";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+
+
 const MyProfile = () => {
+  const {data: session} = useSession();
+
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    const fetchUserDonations = async () => {
+      const response = await fetch(`/api/user/${session?.user.id}/donations`);
+      const data = await response.json();
+      setDonations(data);
+    }
+
+    if (session?.user.id) fetchUserDonations();
+  }, [session?.user.id]);
+  
+
   return (
-    <div>page</div>
+    <div>
+      <UserDonationList 
+        donations = {donations}
+      />
+    </div>
   )
 }
 
